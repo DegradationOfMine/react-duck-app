cebfeeae5a024c13c0dee4a9f96c7b4e3c118d8f
iShabanov <i.s.shabanov@ya.ru>
2019-09-28 23:33:14 +0300

    global containers

--- src/core/containers/RouteContainer.ts
+++ src/core/containers/RouteContainer.ts
@@ -1,7 +1,19 @@
-import Singleton from "../abstraction/Singleton";
-
-export default class RouteContainer extends Singleton {
+export default class RouteContainer {
+    protected static instance: RouteContainer;
+    //
     protected static routes: {[k:string]: object[]};
+
+    private constructor() {
+        RouteContainer.routes = {};
+    }
+
+    static get new(): RouteContainer {
+        if (!RouteContainer.instance) {
+            RouteContainer.instance = new RouteContainer();
+        }
+
+        return RouteContainer.instance;
+    }
 
     public put(module: string, routes: object[]): void {
         const obj = {...RouteContainer.routes};
