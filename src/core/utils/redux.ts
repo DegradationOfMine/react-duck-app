fa84eb232a30a53b5c260767a743a115ab54574a
iShabanov <i.s.shabanov@ya.ru>
2019-09-28 23:35:09 +0300

    some util stuff

--- src/core/utils/redux.ts
+++ src/core/utils/redux.ts
@@ -1,16 +1,15 @@
-export const makeAction: Function = (type: string) => (payload: object = {}) => ({type, payload});
+import Action from "../types/Action";
+import Reducers from "../types/Reducers";
+import State from "../types/State";
+
+export const makeAction = (type: string) => (payload: object = {}) => ({type, payload});
 //
-export const makeReducer: Function = (initialState: object = {}, reducers: object = {}) => {
-    return (state: object = initialState, action: object) => {
+export const makeReducer = (initialState: State = {}, reducers: Reducers = {}) => {
+    return (state: State = initialState, action: Action) => {
         const type = action.type || '';
         if (reducers.hasOwnProperty(type)) {
             return reducers[type](state, action)
         }
         return state;
     }
-};
-//
-export default {
-    makeAction,
-    makeReducer,
 };
